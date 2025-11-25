bdf6db487fffdef62c17cfd5a9874574a33a39fe
realapk-alt <realapk09@gmail.com>
2025-11-25 18:49:34 +0530

    Add files via upload

---
+++ app/config.php
@@ -0,0 +1,6 @@
+<?php
+// Do NOT commit a token to GitHub. This file should be filled on the server only.
+define('GITHUB_USER', 'realapk-alt');
+define('GITHUB_REPO', 'dataPiN');
+define('GITHUB_TOKEN', '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'); // <-- Paste token on your server, not in repo
+?>
